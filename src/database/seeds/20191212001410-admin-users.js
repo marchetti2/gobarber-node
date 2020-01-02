09685f827eb6b8bdc49746ec58ddce0b250980d9
marchetti2 <marchetti2@gmail.com>
2020-01-01 23:35:21 -0300

    first commit

---
+++ src/database/seeds/20191212001410-admin-users.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Mario Luiz',
+          email: 'marchetti2@gmail.com',
+          password_hash: bcrypt.hashSync('123456', 8),
+          provider: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: queryInterface => {
+    return queryInterface.bulkDelete('users', null, {});
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
+import AppointmentController from './app/controller/AppointmentController';
+import ScheduleController from './app/controller/ScheduleController';
+import NotificationController from './app/controller/NotificationController';
+import AvailableController from './app/controller/AvailableController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.updated);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.updated);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+export default routes;
